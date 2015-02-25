dafdc0f8c39d825eb055a8777a8cc64118b47b68
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-02-25 15:24:47 -0500

    Adding winning functionality, also switched X and O images to classes, and switched them in on click. Help from Chris on this part

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
 $(document).ready(function(){
-    var turn = 0;
-    $("td").on('click', function() {
-      var tile = $(this);
-      if(turn==0){
-        turn = 1;
-        tile.css('background-image', 'url(images/O.png)');
-    } else {
-        turn = 0;
-        tile.css('background-image', 'url(images/X.png)');
-    }
-    });
+	var turn = 0;
+	$("td").on('click', function() {
+		var tile = $(this);
+		if(turn==0){
+			turn = 1;
+			tile.addClass("X")
+		} else {
+			turn = 0;
+			tile.addClass("O")
+		}
+		tile.off('click')
 
+		if (winner("X")) {
+		  alert("X wins!")
+		} else if (winner("O")) {
+		  alert("O wins!")
+		}
+	});
 });
+
+
+
+
+wins=[ 
+	[1,2,3], 
+	[4,5,6], 
+	[7,8,9], 
+	[1,4,7], 
+	[2,5,8], 
+	[3,6,9], 
+	[1,5,9], 
+	[3,5,7]
+]
+
+function getCell(n) {
+  return $("#"+n)
+}
+
+function winner(player) {
+	for(var i=0; i < wins.length; i++) {
+		var win = wins[i]
+
+		var allOccupied = true
+		for (var j=0; j < win.length; j++) {
+			var cellNum = win[j]
+			var cell = getCell(cellNum)
+			if (!cell.hasClass(player)){
+				allOccupied = false
+			}
+		}
+		if (allOccupied) return true
+	}
+
+	return false
+}
+
+
+
+
+
+
+
