41e77e29818b92a5a1860788dd1f0186ed8ce65f
Chijioke Okafo <chijiokeokafo@gmail.com>
2015-02-24 19:05:40 -0500

    Adding click functionality to tiles on gameboard, onclick renders either a .png of X or O

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-    $('td').on('click', function(){
-        alert('click');
+    var turn = 0;
+    $("td").on('click', function() {
+      var tile = $(this);
+      if(turn==0){
+        turn = 1;
+        tile.css('background-image', 'url(images/O.png)');
+    } else {
+        turn = 0;
+        tile.css('background-image', 'url(images/X.png)');
+    }
     });
 
 });
